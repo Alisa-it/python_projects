698c46102a0e77660783ebde811eb7894296ff5f
Alisa <askerrr2002@gmail.com>
2023-05-04 22:35:20 +0300

    added vigeners en/de-coder

--- tasks_21-30/task28.py
+++ tasks_21-30/task28.py
@@ -1,7 +1,22 @@
 #  28. Шифр Виженера
+d = [chr(i) for i in range(127)]
+dl = len(d)
 
-alph = 'абвгдеёжзийклмнопрстуфхцчшщьыъэюя'
+prepval = lambda val: zip(range(0, len(val)), val )
+
+enc = lambda ch, key: (ch + key) % dl
+dec = lambda ch, key: (ch - key + dl) % dl
+
+def vigenere(value, key, func):
+    kl = len(key)
+    value = prepval(value)
+    e = [func(ord(c), ord(key[i % kl])) for (i, c) in value]
+    return ''.join([d[c] for c in e])
 
 msg = str(input("Введи сообщение которое хочешь зашифровать - "))
 key = str(input("Введи ключ - "))
 
+tmp = vigenere(msg, key, enc)
+
+print(f'Зашифрованное сообщение - {tmp}')
+print(f'Расшифрованное сообщение - {vigenere(tmp, key, dec)}')
